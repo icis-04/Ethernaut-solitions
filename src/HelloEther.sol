d457ec9afac29265c1843a7aebbb8cbf3508fa32
MACBOOK AIR <user@MACBOOKs-MacBook-Air.local>
2024-11-24 06:01:25 +0100

    src folder updated

---
+++ src/HelloEther.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Instance {
+    string public password;
+    uint8 public infoNum = 42;
+    string public theMethodName = "The method name is method7123949.";
+    bool private cleared = false;
+
+    // constructor
+    constructor(string memory _password) {
+        password = _password;
+    }
+
+    function info() public pure returns (string memory) {
+        return "You will find what you need in info1().";
+    }
+
+    function info1() public pure returns (string memory) {
+        return 'Try info2(), but with "hello" as a parameter.';
+    }
+
+    function info2(string memory param) public pure returns (string memory) {
+        if (keccak256(abi.encodePacked(param)) == keccak256(abi.encodePacked("hello"))) {
+            return "The property infoNum holds the number of the next info method to call.";
+        }
+        return "Wrong parameter.";
+    }
+
+    function info42() public pure returns (string memory) {
+        return "theMethodName is the name of the next method.";
+    }
+
+    function method7123949() public pure returns (string memory) {
+        return "If you know the password, submit it to authenticate().";
+    }
+
+    function authenticate(string memory passkey) public {
+        if (keccak256(abi.encodePacked(passkey)) == keccak256(abi.encodePacked(password))) {
+            cleared = true;
+        }
+    }
+
+    function getCleared() public view returns (bool) {
+        return cleared;
+    }
+}
+Go to the next level
+Get new instance
+Level author(s):
+
+Alejandro Santander
+
+Did this level teach you anything useful? Donate to the level author (on mainnet): 0x31a3801499618d3c4b0225b9e06e228d4795b55d
+d
